699d6c4354de1ff4adfb5dac2582d6079a76048b
Olivia Park <oliviaypark113@gmail.com>
2020-12-18 21:44:17 -0500

    updated js and css html

--- project7.js
+++ project7.js
@@ -1,134 +1,191 @@
 
 //For weather input appID for weather 
-var appID = "80b3e8a297999f6bc99d97f895ecd144"
- var defaultCity = "new york"
- var query_param;
- var currentDay = document.querySelector("#current-date");
- var now = moment().format('LLL');
+  var appID = "80b3e8a297999f6bc99d97f895ecd144"
+  var defaultCity = "new york"
+  var query_param;
+  var currentDay = document.querySelector("#current-date");
+  var now = moment().format('LLL');
+  var cityVariable = localStorage.getItem("cities") || []
 
- var cityVariable = localStorage.getItem("cities") || []
+  lastSavedCity ()
 
- lastSavedcity ()
+  function lastSavedCity(){
+    if(cityVariable){
+          weather ="https://api.openweathermap.org/data/2.5/weather?q=" +cityVariable +"&units=imperial"+"&appid=" + appID;
+      
+        getWeatherData()
+    }
 
-
-function lastSavedcity(){
-
-   if(cityVariable){
-        weather ="https://api.openweathermap.org/data/2.5/weather?q=" +cityVariable +"&units=imperial"+"&appid=" + appID;
-    
-       getWeatherData()
-   }
-
-}
+  }
  
 
 // Indicate Current date and time .. on front page
-function displayCurrentDay() {
-    currentDay.textContent = now;
-}
+  function displayCurrentDay() {
+      currentDay.textContent = now;
+  }
 
-displayCurrentDay();
+  displayCurrentDay();
 
-$(".search_btn").on("click",function(){
+  $(".search_btn").on("click",function(){
 
-  query_param =$(this).prev().val();
+    localStorage.clear()
 
- if($(this).prev().attr("placeholder") == "City"){
-      
-      weather ="https://api.openweathermap.org/data/2.5/weather?q=" + query_param +"&units=imperial"+"&appid=" + appID;
+    query_param =$(this).prev().val();
 
-      getWeatherData()    
- }
+  if($(this).prev().attr("placeholder") == "City"){
+        
+        weather ="https://api.openweathermap.org/data/2.5/weather?q=" + query_param +"&units=imperial"+"&appid=" + appID;
 
- else if($(this).prev().attr("placeholder")=="Zip Code"){
+        getWeatherData()    
+  }
 
-  zipCodeweather ="https://api.openweathermap.org/data/2.5/weather?zip=" + query_param +"&units=imperial"+"&appid=" + appID;
+  else if($(this).prev().attr("placeholder")=="Zip Code"){
 
-   getZipcodeWeatherData()
+    zipCodeweather ="https://api.openweathermap.org/data/2.5/weather?zip=" + query_param +"&units=imperial"+"&appid=" + appID;
 
-}
+    getZipcodeWeatherData()
 
-});
+  }
 
+  localStorage.setItem('cities', query_param)
 
-
-function  getWeatherData() {
-  $.getJSON(weather, function(json){
-
-       weather ="https://api.openweathermap.org/data/2.5/weather?q=" + query_param +"&units=imperial"+"&appid=" + appID;
-
-      $("#city").text(json.name);
-      $("#country").text(" ," + json.sys.country);
-      $("#main_weather").text(json.weather[0].main);
-      $("#description_weather").text(json.weather[0].description);
-      $("#weather_image").attr("src", "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
-      $("#temperature").text(json.main.temp);
-
-  })
-}
+  });
 
 
-function  getZipcodeWeatherData() {
-  $.getJSON(zipCodeweather, function(json){
-       
-      zipCodeweather ="https://api.openweathermap.org/data/2.5/weather?zip=" + query_param +"&units=imperial"+"&appid=" + appID;
-  
-      $("#city").text(json.name);
-      $("#country").text(" ," + json.sys.country);
-      $("#main_weather").text(json.weather[0].main);
-      $("#description_weather").text(json.weather[0].description);
-      $("#weather_image").attr("src", "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
-      $("#temperature").text(json.main.temp);
+  function  getWeatherData() {
+    $.getJSON(weather, function(json){
 
-  })
-}
+        weather ="https://api.openweathermap.org/data/2.5/weather?q=" + query_param +"&units=imperial"+"&appid=" + appID;
 
-var musicVideoURL;
+        $("#city").text(json.name);
+        $("#country").text(" ," + json.sys.country);
+        $("#main_weather").text(json.weather[0].main);
+        $("#description_weather").text(json.weather[0].description);
+        $("#weather_image").attr("src", "https://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
+        $("#temperature").text(json.main.temp);
 
-var numResults = 0;
-var masterList =[];
-
-function runRandom(num){
-  console.log(num)
-  var parseNum = parseInt(num)
-     var flatMasterList = masterList.flat()
-        for (var i=0; i<parseNum; i++){
-        var randomSong =  flatMasterList[Math.floor(Math.random()*flatMasterList.length)]
-        console.log(randomSong)
-        }
-
-}
+    })
+  }
 
 
-
-var clearKeyword;
-var cloudsKeyword;
-
-$("#musicBtn").on("click", async function(){
-
-   numResults = $("#numRecords").val()
+  function  getZipcodeWeatherData() {
+    $.getJSON(zipCodeweather, function(json){
+        
+        zipCodeweather ="https://api.openweathermap.org/data/2.5/weather?zip=" + query_param +"&units=imperial"+"&appid=" + appID;
     
-   var userWeather = $("#main_weather").text()
+        $("#city").text(json.name);
+        $("#country").text(" ," + json.sys.country);
+        $("#main_weather").text(json.weather[0].main);
+        $("#description_weather").text(json.weather[0].description);
+        $("#weather_image").attr("src", "https://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
+        $("#temperature").text(json.main.temp);
 
-   if(userWeather.includes("Clear")||userWeather.includes("Sunny"))
-   { 
-      clearKeyword =["pop","hiphop","country","ska","reggae"];
+    })
+  }
 
-      for(var i =0; i<clearKeyword.length; i++){
-      
-      musicVideoURL = "https://itunes.apple.com/search?term="+clearKeyword[i]+"&media=musicVideo&limit=200"
-      var response = await $.ajax({
-         url: musicVideoURL,
-         method: "GET"
-       })
-     console.log(JSON.parse(response))
-     var responseJson = JSON.parse(response)
-      masterList.push(responseJson.results)
+
+
+  //variables for media APIs 
+
+  var musicVideoURL;
+  var numResults = 0;
+  var masterList =[];
+
+  //DOM element 
   
-     }
+  var musicBtn = $("#musicBtn")
 
-}
+  var container = $(".mv-container")
+  var artistImage = $(".image-artist")  
+  var media = $(".media")
+  var overlay = $(".overlay")
 
- runRandom(numResults);
-})
+  // randomly pulled data based on selected Number 
+
+  function runRandom(num){
+    //number value string convert into " number"
+    var parseNum = parseInt(num)
+      var flatMasterList = masterList.flat()
+          artistImage.empty()
+          for (var i=0; i<parseNum; i++){
+          var randomSong =  flatMasterList[Math.floor(Math.random()*flatMasterList.length)]
+          // console.log(randomSong)// randomly picked objects from master array 
+          // console.log(randomSong.trackName)
+          // console.log(randomSong.artistName)  
+          // console.log(randomSong.primaryGenreName)
+          // console.log(randomSong.trackCensoredName)
+          // console.log(randomSong.previewUrl) //music video
+          // console.log(randomSong.artworkUrl100)  //image 
+
+          // add into our DOM video element ..
+          var img = $("<img>")
+          img.attr("src", randomSong.artworkUrl100)
+          img.attr("id", "artist-image")
+          artistImage.append(img)
+
+        }
+        
+        artistImage.on("click",img , function(e){
+          e.stopPropagation()
+          console.log($(this))
+      
+          openMedia(`${randomSong.previewUrl}, ${randomSong.trackName}`)
+          
+        })
+      }
+      
+
+  function openMedia (url,title) {  
+    console.log(url)
+    media.innerHTML = `<video controls autoplay src="${url}" ></video> <p>${title}</p>`
+  media.removeClass("hidden")};
+  
+
+  function closeMedia(){
+    media.innerHTML = ""
+  }
+ 
+  function toggleOverlay (){
+    overlay.toggleClass("blur")
+    img.each(function(image){
+      image.toggleClass("blur")
+    })
+
+  }
+
+
+  var clearKeyword;    //("Clear")
+  var cloudsKeyword;  //("Clouds")
+  var snowKeyword;   //("Snow")
+  var rainKeyword;  //("Rain")
+  var windKeyword;  //("Windy")
+
+  musicBtn.on("click", async function(){
+
+    numResults = $("#numRecords").val()  
+    var userWeather = $("#main_weather").text()
+
+    if(userWeather.includes("Clear"))
+    { 
+        clearKeyword =["pop","hiphop","country","ska","reggae"];
+
+        for(var i =0; i<clearKeyword.length; i++){
+        
+        musicVideoURL = "https://itunes.apple.com/search?term="+clearKeyword[i]+"&media=musicVideo&limit=200"
+
+          var response = await $.ajax({
+            url: musicVideoURL,
+            method: "GET"
+
+          })
+        // console.log(JSON.parse(response))
+        var responseJson = JSON.parse(response)
+        masterList.push(responseJson.results)
+      
+      }
+
+  }
+
+  runRandom(numResults);
+  })
 
